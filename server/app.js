d9f8bf1cb5f79aab8e2f76a76ea3692e18be1d83
mah-davarfa <mah.davarfa@gmail.com>
2026-02-05 20:39:38 -0500

    feat: add security middleware (cors, helmet, rate limiting)

--- server/app.js
+++ server/app.js
@@ -1,5 +1,52 @@
 import express from "express"
+import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import errorHandler from './middlewares/errorHandler.js'
+import signin from "./controllers/authController.js"
+import signup from './controllers/signupController.js'
 
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(
+    cors({
+    origin:FRONTEND_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+    allowedHeaders: ["Content-Type", "Authorization"]
+    })
+)
+ const authLimiter=rateLimit({
+    windowMs:12*60*60*1000,//12 hours window
+    limit:100,
+    standardHeaders:'draft-7',
+    legacyHeaders:false,
+    message:"Too many request from this IP , pleas try again later"
+ })
+
+// / Landing
+app.get('/',(req,res)=>{
+    res.json({message:"API is running"})
+})
+// /signup
+app.use('/signin',authLimiter,signin)
+// /login
+app.use('/signup',authLimiter,signup)
+
+// /app/groups (groups dashboard)
+// /app/groups/new (create group)
+// /app/groups/:groupId (group detail expenses)
+// /app/groups/:groupId/members (members/invites)
+// /app/groups/:groupId/balances (balance sheet)
+// /app/settings (profile/settings)
+
+//////orute Not Found////
+app.use((req,res)=>{
+    res.status(404).json({error: 'Route not found'})
+})
+///Error Handler
+app.use(errorHandler);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-
-import app from './app.js';
 import dotenv from 'dotenv';
-import connectDB from './config/database.js';
-
 dotenv.config();
+import connectDB from './config/database.js';
+import app from './app.js';
+
 
 const PORT = process.env.PORT || 3011;
 
