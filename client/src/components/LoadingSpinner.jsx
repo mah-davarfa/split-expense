16e67771011a0183b3b33a48bd1260097cd2f5c8
mah-davarfa <mah.davarfa@gmail.com>
2026-03-31 12:30:10 -0400

    loadinig re-designed

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -3,8 +3,11 @@ export default function LoadingSpinner({label='Loading...'}) {
     <div 
     style={{
         padding:12,
-        display:'flex',
-        alignItems:'center',
+        // display:'flex',
+        position:"relative",
+        
+        // width:'100%',
+        // alignItems:'center',
         gap:10,
     }}
     role='status'
@@ -12,17 +15,26 @@ export default function LoadingSpinner({label='Loading...'}) {
     >
       <span
           style={{
-          width: 16,
-          height: 16,
-          border: "2px solid #bbb",
-          borderTopColor: "#333",
-          borderRadius: "50%",
-          display: "inline-block",
-          animation: "spin 0.8s linear infinite",
+           width: 200,
+          //  height: 100,
+          // border: "2px solid red",
+          // borderTopColor: "#333",
+          // borderRadius: "50%",
+          // display: "inline-block",
+          // animation: "spin 0.8s linear infinite",
+          
+          position: 'absolute',
+          inset: 0,
+          transform:' translateX(0%)',
+          background: 'linear-gradient(90deg,transparent, rgba(255,255,255,0.6), transparent)',
+          animation: 'conectTo 1.2s infinite',
         }}
       >{label}</span>
-      <style>
+      {/* <style>
         {`@keyframes spin { to { transform: rotate(360deg); } }`}
+      </style> */}
+      <style>
+        {'@keyframes conectTo {100% { transform: translateX(100%); }}'}
       </style>
     </div>
   );
