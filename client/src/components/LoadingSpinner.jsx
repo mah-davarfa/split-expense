ebe458322a01cfdcb11d74860bddd0b6a79e4a0e
mah-davarfa <mah.davarfa@gmail.com>
2026-04-15 11:23:01 -0400

    loading chage

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -15,27 +15,27 @@ export default function LoadingSpinner({label='Loading...'}) {
     >
       <span
           style={{
-           width: 200,
-          //  height: 100,
-          // border: "2px solid red",
-          // borderTopColor: "#333",
-          // borderRadius: "50%",
-          // display: "inline-block",
-          // animation: "spin 0.8s linear infinite",
+           width: 32,
+           height: 32,
+          border: "2px solid  #ddd",
+          borderTopColor: "#040405",
+          borderRadius: "50%",
+          display: "inline-block",
+          animation: "spin 0.7s linear infinite",
           
-          position: 'absolute',
-          inset: 0,
-          transform:' translateX(0%)',
-          background: 'linear-gradient(90deg,transparent, rgba(255,255,255,0.6), transparent)',
-          animation: 'conectTo 1.2s infinite',
+          // position: 'absolute',
+          // inset: 0,
+          // transform:' translateX(0%)',
+          // background: 'linear-gradient(90deg,transparent, rgba(255,255,255,0.6), transparent)',
+          // animation: 'conectTo 1.2s infinite',
         }}
-      >{label}</span>
-      {/* <style>
-        {`@keyframes spin { to { transform: rotate(360deg); } }`}
-      </style> */}
+      ></span>
       <style>
-        {'@keyframes conectTo {50% { transform: translateX(50%); }}'}
+        {`@keyframes spin { to { transform: rotate(360deg); } }`}
       </style>
+      {/* <style>
+        {'@keyframes conectTo {50% { transform: translateX(50%); }}'}
+      </style> */}
     </div>
   );
 }
